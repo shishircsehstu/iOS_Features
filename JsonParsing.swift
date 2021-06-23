78734eee450f32f9981a7add087d52258a640350
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2021-06-23 18:56:30 +0600

    Create JsonParsing.swift

---
+++ JsonParsing.swift
@@ -0,0 +1,44 @@
+
+
+import Foundation
+
+/// Make model according to json formetter
+
+struct allData: Codable{
+    var myData: [allItems]
+    var status: String
+}
+
+struct allItems: Codable{
+    var title: String
+    var items: [String]
+}    
+
+
+func fetchData(){
+        
+        guard let path = Bundle.main.path(forResource: "myData", ofType: ".json") else{
+            return
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        do{
+            let jsonData = try Data(contentsOf: url)
+            allDataInfo = try JSONDecoder().decode(allData.self, from: jsonData)
+            guard let allDataInfo = allDataInfo else{ return }
+            print(allDataInfo.myData.count)
+            print("status :: ",allDataInfo.status )
+            
+            for itm in allDataInfo.myData{
+                
+                print(itm.title," :: ")
+                for items in itm.items{
+                    print(items)
+                }
+                print("\n")
+            }
+        }
+        catch{
+            print(error.localizedDescription)
+        }
+    }
