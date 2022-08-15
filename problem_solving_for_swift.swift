e2e6566aa3dbd7f2d2e014d0223c283d3116cd89
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2022-08-15 16:49:44 +0600

    Update problem_solving_for_swift.swift

--- problem_solving_for_swift.swift
+++ problem_solving_for_swift.swift
@@ -1,2 +1,8 @@
+//init 1D array
+var isVisited = [Int](repeating: 0, count: 100)
+
 // Init 2D array
 var dp = [[Int]](repeating: [Int](repeating: -1, count: 5003), count: 303)
+
+// Find ascci value
+var val = Int(yourChar.asciiValue!)
