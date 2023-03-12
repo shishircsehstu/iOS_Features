6f0c04e457514c78b7d258884c06f7821a6dcca3
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2023-03-12 16:57:14 +0600

    Update lazy.swift

--- lazy.swift
+++ lazy.swift
@@ -1,1 +1,28 @@
 // A lazy stored property is a property whose initial value isn't calculated until the first time it's used.
+
+class Device{
+    
+    var name = String()
+    lazy var lazyValue: Int = {
+        var cnt=0
+        for i in 1...3{
+            cnt+=i
+            print("Lazy :",cnt)
+        }
+        print("Lazy called")
+        return cnt
+    }()
+    
+    var nonLazyValue: Int {
+       
+        var cnt=0
+        for i in 1...3{
+            cnt+=i
+            print("non lazy: ",cnt)
+        }
+       
+        return cnt
+    }
+    
+}
+
