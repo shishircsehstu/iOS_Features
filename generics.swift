c3400a6519f70c972ded1a4b61816e9ad0c23823
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2023-09-06 12:38:48 +0600

    Create generics.swift

---
+++ generics.swift
@@ -0,0 +1,8 @@
+#Generics used write code that works for multiple types and specify requirements for those types.
+
+func getMaxVal<T: Comparable>(a: T, b: T)->T{
+    return a>b ? a:b
+}
+
+print(getMaxVal(a: "abc", b: "xyz")) // xyz
+print(getMaxVal(a: 12, b: 12.4))  // 12.4
