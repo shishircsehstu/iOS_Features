821137483cd07890a79e79c00cb11e1a7fe5acf9
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2024-12-08 20:08:32 +0600

    Update keyword.swift

--- keyword.swift
+++ keyword.swift
@@ -3,93 +3,25 @@ Here is a complete list of all reserved keywords in Swift, categorized and witho
 
 # Declaration Keywords
 associatedtype
-class
-deinit
-enum
-extension
-func
-import
-init
 inout
-let
-operator
-protocol
-static
-struct
 subscript
-typealias
-var
-# Control Flow Keywords
-break
-case
-continue
-default
 defer
-do
-else
 fallthrough
-for
-guard
-if
-in
 repeat
-return
-switch
-where
-while
-# Error Handling Keywords
-catch
-throw
-throws
-rethrows
-try
-Asynchronous and Concurrency Keywords
 actor
-async
 await
 nonisolated
+dynamic
 Task
-Type and Casting Keywords
-as
-Any
-AnyObject
-is
-nil
-Self
-Protocol
-Type
-# Memory Management Keywords
-weak
-unowned
-lazy
-# Access Control Keywords
 open
 public
 internal
 private
 fileprivate
-Contextual Keywords
 super
-self
-true
-false
-# Operator Keywords
 infix
 prefix
 postfix
-Attributes
-@available
-@discardableResult
-@dynamicCallable
-@dynamicMemberLookup
-@frozen
-@objc
-@objcMembers
-@propertyWrapper
-@resultBuilder
-@testable
-@main
-# Compilation and Debugging Keywords
 #available
 #colorLiteral
 #column
@@ -110,7 +42,7 @@ Attributes
 Special Literals
 #keyPath
 #selector
-# New Concurrency Keywords (Swift 5.5+)
+
 Sendable
 isolated
 MainActor
